0153ce930ea35ef1518824d9bd88463279d9c41c
Aniket taneja <anikettaneja124@gmail.com>
2025-10-21 13:39:51 +0530

    Updated intro

--- pages/Home.py
+++ pages/Home.py
@@ -7,8 +7,8 @@ if "name" in st.session_state and "email" in st.session_state:
 #Page content
 st.title("Welcome to Streamlytix")
 st.markdown("""
-  <p>This is a data analaysis project built with the help of Python and its various libraries and frameworks mainly Pandas, Numpy, Matplotlib, Seaborn, Plotly-express. The project's ui is built with the help of Streamlit.</p>
-  <p>Main focus of this project is to provide isights to user about his/her content consumption insights. The goal is to provide user with the details about his/her watch patterns in the simplest way possible.</p>
+  <p>This is a data analaysis, machine learning project built with the help of Python and its various libraries and frameworks mainly Pandas, Numpy, Matplotlib, Seaborn, Plotly-express, Schikit-Learn. The project's ui is built with the help of Streamlit.</p>
+  <p>Main focus of this project is to provide insights to user about his/her content consumption and recommendations. The goal is to provide user with the details about his/her watch patterns in the simplest way possible and providing user with recommendations of what to watch next.</p>
   <p>Our platform consists of a vast collection of movies and tv shows (10K+) to provide better user experience for everyone. </p>
 """,unsafe_allow_html=True)
 btn = st.button("Get started",type="primary")
